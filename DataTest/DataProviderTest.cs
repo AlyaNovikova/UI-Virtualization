337e4d007f72944024af2b932a3c0db0ded0b97c
Alya Novikova <aleksnovikova@gmail.com>
2020-12-27 15:01:28 +0300

    add tests for database

---
+++ DataTest/DataProviderTest.cs
@@ -0,0 +1,55 @@
+﻿using DogData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace DataTest
+{
+    [TestClass]
+    public class DataProviderTest
+    {
+        [TestMethod]
+        public void TestNumberOfDogs()
+        {
+            int cnt = DogDataProvider.NumberOfDogs();
+  
+            Assert.IsTrue(cnt > 0);
+        }
+
+        [TestMethod]
+        public void TestDataSegment()
+        {
+            var dogs = DogDataProvider.DataSegment(0, 10);
+
+            Assert.IsTrue(dogs != null && dogs.Count == 10);
+        }
+
+        [TestMethod]
+        public void TestAllDogs()
+        {
+            var dogs = DogDataProvider.AllDogs();
+            Assert.IsTrue(dogs != null && dogs.Count > 0);
+        }
+
+        [TestMethod]
+        public void TestDogById()
+        {
+            var dogs = DogDataProvider.AllDogs();
+            Assert.IsTrue(dogs != null && dogs.Count > 0);
+
+            foreach (var dog in dogs)
+            {
+                int dogId = dog.DogId;
+                var dogById = DogDataProvider.DogById(dogId);
+
+                Assert.IsTrue(dogById != null);
+                Assert.IsTrue(dogById.DogId == dogId);
+
+                Assert.IsTrue(dog.Breed != null);
+                Assert.IsTrue(dog.Image_data != null);
+
+            }
+        }
+    }
+}
+
---
+++ DataTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5f01e71-26ee-40fe-ab7f-fa9d42b2ee3d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
