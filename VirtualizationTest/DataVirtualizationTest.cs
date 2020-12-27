0b9232902b24229a80302e2ffcb363fe091666c2
Alya Novikova <aleksnovikova@gmail.com>
2020-12-27 15:25:01 +0300

    add tests for virtualization

---
+++ VirtualizationTest/DataVirtualizationTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DogData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Virtualization;
+
+namespace VirtualizationTest
+{
+    [TestClass]
+    public class DataVirtualizationTest
+    {
+        [TestMethod]
+        public void CountTest()
+        {
+            DogIDataMock dogData = new DogIDataMock(10, 0);
+            var controller = new DataVirtualization<DogMock>(dogData, 1, 10000, 10);
+
+            Assert.IsTrue(controller.Count == 10);
+        }
+
+        [TestMethod]
+        public void GetByIdTest()
+        {
+            DogIDataMock dogData = new DogIDataMock(10, 0);
+            var controller = new DataVirtualization<DogMock>(dogData, 1, 10000, 10);
+
+            Assert.IsTrue(controller.Count == 10);
+
+            for (int i = 0; i < controller.Count; i++)
+            {
+                Assert.IsTrue(controller[i] != null);
+            }
+        }
+    }
+}
---
+++ VirtualizationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VirtualizationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VirtualizationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6051602d-0f84-47a0-8c04-7d48b75bb054")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
